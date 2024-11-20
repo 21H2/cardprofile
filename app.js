44f93a1832283d53c440fa9bd491b3e74004065b
Reappointed <83266784+21H2@users.noreply.github.com>
2024-11-20 10:51:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,10 +12,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   type();
-
-  // Theme Toggle
-  const themeToggle = document.getElementById('themeToggle');
-  themeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-  });
-});
